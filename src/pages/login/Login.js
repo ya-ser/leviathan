593eefe3a417615fd4866b9c25ea336a60a77329
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-02-15 16:55:39 -0500

    initial setup of login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,9 +1,40 @@
+import { useState } from 'react'
 import './Login.css'
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password);
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password}
+        />
+      </label>
+      <button>Login</button>
+      {/* {!isPending && <button className="btn">Login</button>}
+      {isPending && <button className="btn" disabled>loading</button>}
+      {error && <div className="error">{error}</div>} */}
+    </form>
   )
 }
