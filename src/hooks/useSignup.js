358fbdd10bcd4765ec41bd9969f841fc86b003e8
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-02-15 15:46:43 -0500

    Create users collection in firebase

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { projectAuth, projectStorage } from '../firebase/config'
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
@@ -27,6 +27,13 @@ export const useSignup = () => {
 
       // add display name to user
       await res.user.updateProfile({ displayName, photoURL: imgUrl })
+
+      // create firestore user document for every user that signs up, the id of the doc is the user id, then set the data on the doc
+      await projectFirestore.collection('users').doc(res.user.uid).set({
+        online: true,
+        displayName,
+        photoURL: imgUrl
+      })
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
