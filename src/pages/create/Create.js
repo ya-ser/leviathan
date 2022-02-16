0e97a78a3064a9efbaf6de9e0e8d10719551ad38
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-02-16 11:49:43 -0500

    Add task form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,9 +1,66 @@
+import { useState } from 'react'
 import './Create.css'
 
+
 export default function Create() {
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  // category test
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(name, details, dueDate)
+  }
+
+
   return (
-    <div>
-      Create
+    <div className='create-form'>
+      <h2 className='page-title'>Create a new task</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Task name:</span>
+          <input 
+            required 
+            type="text" 
+            onChange={(e) => setName(e.target.value)} 
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Task details:</span>
+          <textarea
+            required 
+            type="text" 
+            onChange={(e) => setDetails(e.target.value)} 
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required 
+            type="date" 
+            onChange={(e) => setDueDate(e.target.value)} 
+            value={dueDate}
+          />
+        </label>
+        <label>
+          // this is a category test
+          <span>Task category:</span>
+          {/* category select */}
+        </label>
+        <label>
+          <span>Assign to:</span>
+          {/* assignee select */}
+        </label>
+
+        <button className='btn'>Add Task</button>
+      </form>
+    
     </div>
   )
 }
