ed75c42323c018bf9b5630be96344db984c94e14
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-02-15 20:00:09 -0500

    Show or hide login/signup in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,21 +3,36 @@ import './Navbar.css'
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
+
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
+
   return (
     <div className='navbar' >
       <ul>
         <li className="logo">
           <img src={logo} alt="leviathan logo" />
         </li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li>
-          {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-          {isPending && <button className="btn" disabled>Logging out...</button>}
-        </li>
+
+        {!user && (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+          </>
+        )}
+
+        {user && (
+
+          <li>
+            {!isPending && <button className="btn" onClick={logout}>Logout</button>}
+            {isPending && <button className="btn" disabled>Logging out...</button>}
+          </li>
+
+        )}
+
       </ul>
     </div>
   )
