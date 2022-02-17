ea701698305d89650aa0b0fe3a656051d8558899
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-02-17 14:44:28 -0500

    filter functionality completely applied

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,15 +3,38 @@ import { useCollection } from '../../hooks/useCollection'
 import './Dashboard.css'
 import TaskFilter from './TaskFilter'
 import { useState } from 'react'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 
 export default function Dashboard() {
+  const { user } = useAuthContext()
   const { documents, error } = useCollection('tasks')
   const [currentFilter, setCurrentFilter] = useState('all')
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter)
   }
+
+  const tasks = documents ? documents.filter((document) => {
+    switch(currentFilter) {
+      case 'all':
+        return true
+      case 'mine':
+        let assignedToMe = false
+        document.assignedUsersList.forEach((u) => {
+          if(user.uid === u.id) {
+            assignedToMe = true
+          }
+        })
+        return assignedToMe
+      case 'development':
+      case 'design':
+        console.log(document.category, currentFilter)
+        return document.category === currentFilter
+      default:
+        return true
+    }
+  }) : null
 
   return (
     <div>
@@ -20,7 +43,7 @@ export default function Dashboard() {
       {documents && (
         <TaskFilter currentFilter={currentFilter} changeFilter={changeFilter}/>
       )}
-      {documents && <TaskList tasks={documents} />}
+      {tasks && <TaskList tasks={tasks} />}
     </div>
   )
 }
